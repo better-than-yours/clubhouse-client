cc57806e47350b09a4994687a7825605a50d059f
Sergei Kuvakin <sergey.kuvakin@coolbet.com>
2021-02-18 19:34:50 +0200

    feat: improvements

--- frontend/src/clubhouse/HowItWorks.tsx
+++ frontend/src/clubhouse/HowItWorks.tsx
@@ -1,8 +1,8 @@
-import { Link } from '@material-ui/core';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Link from '@material-ui/core/Link';
 import React from 'react';
 
 interface Props {
